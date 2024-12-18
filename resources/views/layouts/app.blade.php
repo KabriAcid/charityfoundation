249f6d2cada040246b6ce8917ca7786e04c678c8
Abdullahi Kabri <kabriacid01@gmail.com>
2024-12-18 21:34:29 +0100

    created footer and navbar blade templates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,49 +8,17 @@
     @vite(['resources/css/app.css', 'resources/css/style.css', 'resources/js/app.js'])
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('home') }}">
-                <img src="{{ asset('favicon.png') }}" alt="Logo" height="30">
-                Charity Foundation
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('about') }}">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('programs') }}">Programs</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('news') }}">News</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('contact') }}">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="text-white nav-link btn btn-primary" href="{{ route('donate') }}">Donate</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+     <!-- Include Navbar -->
+    @include('partials.navbar')
 
     <!-- Main Content -->
-    <main class="container py-4">
+    <div class="container">
         @yield('content')
-    </main>
+    </div>
 
     <!-- Footer -->
-    <footer class="py-3 text-center bg-light">
-        <p>&copy; {{ date('Y') }} Charity Foundation. All Rights Reserved.</p>
-    </footer>
+    @include('partials.footer')
+   
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="py-3 text-center bg-light">
+    <p>&copy; {{ date('Y') }} Charity Foundation. All Rights Reserved.</p>
+</footer>
